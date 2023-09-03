9541d07ea2f9b96e1213a01b2d5c69a186c09a38
Jonefg0 <jothagoo@gmail.com>
2023-09-03 15:20:13 -0300

    create PageViews CR

--- webplayground/core/urls.py
+++ webplayground/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import HomePageView, SamplePageView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('sample/', views.sample, name="sample"),
+    path('', HomePageView.as_view(), name="home"),
+    path('sample/', SamplePageView.as_view(), name="sample"),
 ]
